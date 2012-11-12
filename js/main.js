78e7787a23abb50a598458c8c8d62ed4688c1c8c
bugerman <aelfer23@yandex.ru>
2012-11-12 14:01:07 +0300

    files

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$(function () {
+    $('.menu li a').click(function () {
+        if ($(this).parent().find('>ul, >div>ul').size() > 0) {
+            $(this).parent().find('>ul, >div>ul').slideToggle(500);
+            return false;
+        }
+        else {
+            return true;
+        }
+
+    });
+
+    $('.menu li.active').parents('ul').show(0);
+    $('.logo').animate({top:-15}, {duration:2000, easing:'easeOutBounce'});
+
+    $('.scroll').jScrollPane({
+        showArrows:true,
+        arrowScrollOnHover:true
+    });
+
+    $(window).resize(function () {
+        $('.scroll').jScrollPane({
+            showArrows:true,
+            arrowScrollOnHover:true
+        });
+    });
+
+    $('.gallery').jCarouselLite({
+        btnNext:".next",
+        btnPrev:".prev",
+        auto:3000,
+        speed:1000
+
+    });
+
+
+    $('.fancybox').fancybox();
+
+
+});
